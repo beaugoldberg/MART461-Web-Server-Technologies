5d24854bf7bd1fe11437c9f1d559b32321459216
Beau Goldberg <35784775+beaugoldberg@users.noreply.github.com>
2020-11-01 14:49:14 -0700

    Add files via upload

---
+++ assignment-11/Esports Database Scripts/stored-procedures.sql
@@ -0,0 +1,35 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spDisplayAllUsers`()
+BEGIN
+SELECT firstName, lastName, username, gamertag, phoneNum FROM users;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spDisplayUserByUserID`(in user_id int)
+BEGIN
+SELECT * FROM users WHERE userID = user_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spDeleteUserByUserID`(in user_id int)
+BEGIN
+DELETE FROM users WHERE userID = user_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spInsertNewUser`(in firstname varchar(50), in lastname varchar(50), in username varchar(50), in passwrd varchar(50), in gamertag varchar(50), 
+									in phonenum varchar(50))
+BEGIN
+INSERT INTO users (firstName, lastName, username, pwd, gamertag, phoneNum, subscribedchk, adminchk) VALUES (firstname, lastname, username, passwrd, gamertag, phoneNum, 0, 0);
+END$$
+DELIMITER ;DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spValidateUser`(in user_name varchar(50), in pwd varchar(50))
+BEGIN
+SELECT userID FROM users WHERE username = user_name AND pwd = pwd;
+END$$
+DELIMITER ;
+
